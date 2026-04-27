68366fddacd1c83636cacd7e0cd3443994f58fda
gameloque <gabrielhenriqueff1448@gmail.com>
2026-04-27 11:05:16 -0300

    Finalizado o Painel de membros

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int main() {
     float nota1, nota2, presenca, media;
     
+    printf(" BEM VINDO AO SISTEMA DE NOTAS FIAP... \n");
+    printf(" VAMOS COMECAR   \n");
     printf("     PAINEL DE NOTAS MANEIRAS   \n");
     printf("Digite a primeira nota: ");
         scanf("%f", &nota1);
@@ -10,6 +12,8 @@ int main() {
         scanf("%f", &nota2);
     printf("Digite a sua frequencia (0 a 100): ");
         scanf("%f", &presenca);
+    printf("    -------------------------     \n");
+
 
 
     media = (nota1 + nota2) / 2;
