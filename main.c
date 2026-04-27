d00e2ff68b7738022b9e6caaa4aeb156594c0871
osaybtw <teyretteb@gmail.com>
2026-04-27 19:04:31 -0300

    Ajustes

--- main.c
+++ main.c
@@ -1,21 +1,60 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main() {
-    
-    float nota1, nota2, media;
+    char nome[50];
+    int rm;
+    float nota1, nota2, presenca, media;
+    int situacao;
 
+    printf("BEM VINDO AO SISTEMA DE NOTAS FIAP...\n");
+    printf("VAMOS COMECAR\n");
+    printf("PAINEL DE NOTAS\n");
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+
+    printf("Digite seu RM: ");
+    scanf("%d", &rm);
+
+do {
     printf("Digite a primeira nota: ");
-        scanf("%f", &nota1);
+    scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-        scanf("%f", &nota2);
+    scanf("%f", &nota2);
+
+    printf("Digite a sua frequencia (0 a 100): ");
+    scanf("%f", &presenca);
+
+    if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || presenca < 0 || presenca > 100) {
+        printf("Valores invalidos! Digite notas de 0 a 10 e frequencia de 0 a 100.\n");
+    }
+} while (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || presenca < 0 || presenca > 100);
+
+    printf("-------------------------\n");
 
     media = (nota1 + nota2) / 2;
 
-    if (media >= 6) {
-        printf("Aluno aprovado\n");
+    printf("%s %d -> Media geral: %.2f\n", nome, rm, media);
+    printf("-------------------------\n");
+
+    if (media >= 6 && presenca > 66) {
+        situacao = 1;
     } else {
-        printf("Aluno reprovado\n");
+        situacao = 2;
+    }
+
+    switch (situacao) {
+        case 1:
+            printf("Aluno aprovado!\n");
+            break;
+
+        case 2:
+            printf("Aluno reprovado!\n");
+            break;
+
+        default:
+            printf("Erro na verificacao\n");
     }
 
     return 0;
