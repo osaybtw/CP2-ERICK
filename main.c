582d32564b304ad666d34c8ca93c114462396dc6
gameloque <gabrielhenriqueff1448@gmail.com>
2026-04-27 10:53:08 -0300

    Dupliquei o código corrigido

--- main.c
+++ main.c
@@ -1,26 +1,22 @@
-#include   <stdio.h>
+#include <stdio.h> 
 
 int main() {
     
     float nota1, nota2, media;
-    printf()
+
     printf("Digite a primeira nota: ");
         scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-        scanf("%f", nota2);
+        scanf("%f", &nota2);
 
     media = (nota1 + nota2) / 2;
 
-    if (media = 6) {
+    if (media >= 6) {
         printf("Aluno aprovado\n");
-
     } else {
-
         printf("Aluno reprovado\n");
     }
 
     return 0;
-
-    
 }
